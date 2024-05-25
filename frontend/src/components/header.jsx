f0a4ad5392fcb8c95fc8f80ced2b043eb7e57c8c
Adomas Dagys <puslys.adomas@gmail.com>
2024-05-25 20:57:21 +0200

    Add products and individual product page

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,5 +1,6 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { LinkContainer } from 'react-router-bootstrap';
 import logo from '../assets/logo.png'
 
 import React from 'react'
@@ -9,19 +10,26 @@ const header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">
-            <img src={logo} alt='shopLogo' />
-            shop
-          </Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>
+              <img src={logo} alt='shopLogo' />
+              shop
+            </Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav"/>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className='ms-auto'>
-              <Nav.Link href='/cart'>
-                <FaShoppingCart /> Cart
-              </Nav.Link>
-              <Nav.Link href='/login'>
-                <FaUser /> Sign in
-              </Nav.Link>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <FaShoppingCart /> Cart
+                </Nav.Link>
+              </LinkContainer>
+              
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <FaUser /> Sign in
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
